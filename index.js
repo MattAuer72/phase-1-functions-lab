7ebb34fb3303ded26d05054f708ae9ec52779ee0
Matthew Auer <matthewauer72@gmail.com>
2023-03-21 22:27:26 -0500

    functions lab completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+if (block >= 42) {return block - 42}
+else {return 42 - block}
+}
+//distanceFromHqInBlocks()
+
+function distanceFromHqInFeet(block) {return distanceFromHqInBlocks(block) * 264
+}  
+//distanceFromHqInFeet()
+
+function distanceTravelledInFeet(starting, ending) {
+    if (starting >= ending) {return (starting - ending) * 264}
+    else {return (ending - starting) * 264}
+}
+//distanceTravelledInFeet(50, 58)
+
+
+
+function calculatesFarePrice(starting, ending) {
+    
+let distance = starting - ending
+    if (distance < 0 ) { distance = ending - starting}
+    else (distance = starting - ending)
+
+    if ((distance) * 264 <= 400) {
+        return 0}
+    else if ((distance) * 264 > 400 && (distance) * 264 <= 2000)  {
+        return (((distance)* 264) - 400) * .02}
+        else if ((distance) * 264 > 2000 && (distance) * 264 <= 2500) {
+        return 25}
+    else if ((distance) * 264 > 2500) {
+        return 'cannot travel that far'}
+} 
+calculatesFarePrice(43, 44)
+calculatesFarePrice(34, 32)
+calculatesFarePrice(58, 50)
+calculatesFarePrice(34, 24)
+
+
